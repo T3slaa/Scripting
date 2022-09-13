cd6301fae2cc2507424801fefac9f07e4c51a0d8
T3slaa <codex_150@protonmail.com>
2022-09-13 00:36:32 -0300

    Correcion nombre de variables [mal escrito]

--- PasswordGenerator/script.py
+++ PasswordGenerator/script.py
@@ -3,11 +3,11 @@
 import random
 
 # Global Variables 
-caracters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x']
-caracters_speciales = ['#', '!', '@', '$', '%', '^', '&', '}', '{']
+characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x']
+special_characters = ['#', '!', '@', '$', '%', '^', '&', '}', '{']
 numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
-password_length = int(input("input the password lenght:")) 
-string = caracters + caracters_speciales + numbers
+password_length = int(input("input the password lenght:"))
+string = characters + special_characters + numbers
 
 def password_generator(string, password_length):
     for i in range(password_length):
